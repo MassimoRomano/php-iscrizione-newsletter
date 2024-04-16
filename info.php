76b313135d32f116f183b68d5d75606c6f1700a4
MassimoRomano <romano93m@gmail.com>
2024-04-16 18:40:29 +0200

    fix link and add info contact

--- info.php
+++ info.php
@@ -1,13 +1,18 @@
-<header>
+
     <?php
-    include_once __DIR__ . '/../partials/head.php';
+    include_once __DIR__ . '/assets/head.php';
     ?>
-</header>
-<main>
-    <div class="container">
-        <div class="d-flex justify-content-center align-item-center">
-            <h1>Chi Siamo</h1>
+
+<main class="info-main">
+    <div class="container mt-5">
+        <div class=" flex-column justify-content-center align-item-center">
+            <h1 class="text-center mt-5">Chi Siamo</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium pariatur commodi magni mollitia aspernatur! Expedita dolorum reiciendis tenetur ducimus corporis iste, cupiditate necessitatibus id, quasi voluptates dolor placeat possimus. Illum, ipsa? Modi, ratione, dicta quia culpa asperiores dolor incidunt in nemo quod impedit vel eius molestiae recusandae ipsum consectetur laborum temporibus suscipit explicabo hic quas molestias! Illum, iusto consequuntur! Pariatur nam sapiente voluptatem quos! Distinctio facere rem nisi consectetur obcaecati placeat nemo, suscipit quae! Rerum facilis neque quod ea ex, magni quae explicabo laborum laboriosam, rem ab? Reiciendis in magnam fugiat voluptatibus sunt velit, architecto vel odit incidunt necessitatibus hic.</p>
+        </div>
+        <div class="form-group">
+            <label for="my-textarea mt-5">Inviaci un messaggio</label>
+            <textarea id="my-textarea" class="form-control mt-2" name="" rows="3"></textarea>
+            <button class="btn btn-dark mt-3" type="submit">Invia</button>
         </div>
     </div>
 </main>
@@ -15,6 +20,6 @@
 
 <footer>
     <?php
-    include_once __DIR__ . '/../partials/foot.php'
+    include_once __DIR__ . '/assets/foot.php'
     ?>
 </footer>
